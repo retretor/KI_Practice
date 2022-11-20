59365771a76263565f6bb593439c40e272a2815c
retretor <94197381+retretor@users.noreply.github.com>
2022-11-20 23:39:40 +0200

    Lab 3

--- KI_Practice/Labs/DataTypes/DataTypes/IOnewayList.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace KI_Practice.Labs.DataTypes.DataTypes;
-
-public interface IOnewayList<T>
-{
-    void Add(T data);
-    void Remove(T value);
-    void Clear();
-}
--- KI_Practice/Labs/DataTypes/DataTypes/MyStack/IStack.cs
+++ KI_Practice/Labs/DataTypes/DataTypes/MyStack/IStack.cs
@@ -6,5 +6,4 @@ public interface IStack<T>
     T Pop();
     bool CanPush();
     bool CanPop();
-
 }
